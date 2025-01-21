f18920b6d0bc43d0aa61d8796dcebb797889fa0a
kenny.cheung <>
2025-01-21 19:13:33 +0800

    split matrix data width into integer and fractional part; fix matrix data error

--- transformation_matrix_3x3.sv
+++ transformation_matrix_3x3.sv
@@ -2,14 +2,20 @@
 
 module transformation_matrix_3x3 #(
     parameter  IDW = 10,  // input data width
-    parameter  MDW = 16,  // matrix data width
+    parameter  MIDW = 2,  // matrix integer part data width
+    parameter  MFDW = 14,  // matrix fractional part width
     parameter  ODW = 8,   // output data width
+    localparam MDW = MIDW + MFDW,  // matrix total data width
     localparam DIM = 3
 ) (
     input                                    clk,
     input                                    rstn,
+    
+    input                                    in_valid,
     input         [DIM*IDW-1:0]              din,
-    input  signed [DIM*MDW-1:0][DIM*MDW-1:0] matrix,  // [row][column]
+    input signed [DIM-1:0][DIM-1:0][MDW-1:0] matrix,  // [row][column][data]
+
+    output                                   out_valid,
     output        [DIM*ODW-1:0]              dout
 );
 
@@ -23,20 +29,25 @@ module transformation_matrix_3x3 #(
  * 
  */
 
-localparam signed SHIFT_WIDTH = MDW + (IDW - ODW);
+localparam signed SHIFT_WIDTH = MFDW + (IDW - ODW);
 initial begin
-    assert(ODW >= MDW + IDW);
-    assert(SHIFT_WIDTH > 0);
+    assert(MIDW >= 2);  // 1 bit for sign, 1 bit for representing 1.0
+    assert(MFDW >= 1);
+    assert(MDW + IDW >= ODW);
+    assert(SHIFT_WIDTH >= 0);
 end
 
 localparam DIMW = $clog2(DIM);  //DIM data width
 localparam [DIMW-1:0] PAD_DIMW = '0;
 localparam [ODW-1:0] PAD_ODW = '0;
+localparam [MIDW-1:0] PAD_MIDW = '0;
 wire signed [IDW+MDW-1:0] w_prod_row[DIM][DIM];
 logic signed [IDW+MDW+DIMW-1:0] r_sum_row[DIM];
 logic signed [ODW-1:0] r_shifted_row[DIM];
-
+logic [2:0] r_valid = '0;
 genvar idx;
+
+assign out_valid = r_valid[1];
 
 function [ODW-1:0] right_shift;
     input signed [IDW+MDW+DIMW-1:0] data;
@@ -49,11 +60,19 @@ function [ODW-1:0] right_shift;
     logic signed [TEMP_WIDTH:0] temp;
 
     shift = data[SHIFT_WIDTH +: IDW+MDW+DIMW-SHIFT_WIDTH];
-    temp  = shift + $signed({PAD_DIMW, bias});
+    temp  = shift + $signed({PAD_DIMW, PAD_MIDW, bias});
     if (temp >= OUT_MAX) right_shift = '1;
     else if (temp < OUT_MIN) right_shift = '0;
     else right_shift = temp[0 +: ODW];
 endfunction
+
+always_ff @(posedge clk or negedge rstn) begin
+    if (rstn) begin
+        r_valid <= '0;
+    end else begin
+        r_valid <= {r_valid[0 +: 2], in_valid};
+    end
+end
 
 generate
     assign w_prod_row[0][0] = $signed({1'b0, din[0*IDW+:IDW]}) * $signed(matrix[0][0]);
